88d58178c7339967b43c0e2d6e86fe8c11547ee9
Vivek Dutt <vivekduttbudhodi@gmail.com>
2025-10-22 13:14:21 +0530

    Font updated

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,15 @@ import "@fontsource/inter/400.css"; // Regular
 import "@fontsource/inter/500.css"; // Medium
 import "@fontsource/inter/700.css"; // Bold
 
+// src/main.tsx
+import './index.css';
+
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:wght@400;500;700&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Inter", "ui-sans-serif", "system-ui"],
+       fontFamily: {
+        bricolage: ['Bricolage Grotesque', 'sans-serif'],
       },
       animation: {
   'pulse-slow': 'pulse 6s ease-in-out infinite',
